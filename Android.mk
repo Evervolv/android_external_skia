3ddcb4a2a0358da21246533617aa0c01bff4b260
Xin Qi <xqi@codeaurora.org>
2011-11-16 10:06:50 -0800

    Assembly optimized 4 blittering related functions:
    S32A_D565_Opaque
    S32A_Opaque_BlitRow32
    nofilter_gether
    S32A_Blend_BlitRow32
    
    * S32A_D565_Opaque:
    
    Use neon instructions to optimize BLIT routine which operates on
    32-bit source ARGB pixel and 16-bit destination RGB pixel. Here,
    we process 16-pixels in an iteration. We use regular (32-bit)
    arm instructions when number of pixels is less than 16 or the
    architecture doesn't support SIMD/NEON instructions. When alpha
    value is 255, this version of BLIT routine performs 35% better
    in comparison to S32A_D565_Opaque_neon routine.
    
    * S32A_Opaque_BlitRow32 and nofilter_gether:
    
    Use neon instructions to optimize BlitRow32 and nofilter_gether
    functions for 32-bit source and destination pixel arrays. An improvement
    of 76% is observed with neon optimized BlitRow32 function and 6%
    improvement is observed with nofilter_gether function.
    
    * S32A_Blend_BlitRow32:
    
    Optimize S32A_Blend_BlitRow32 using neon instructions for armv7
    based targets and using regular arm instructions for armv6 based
    targets.
    
    This is the consolidated commit from several patches.
    
    Change-Id: Ied93ffc9226415af979d9f7069c201f03a94733a

--- Android.mk
+++ Android.mk
@@ -217,6 +217,30 @@ endif
 LOCAL_SRC_FILES += \
 	emoji/EmojiFont.cpp
 
+
+# including the optimized assembly code for the src-overing operation
+ifeq ($(TARGET_ARCH),arm)
+	LOCAL_CFLAGS += -D__CPU_ARCH_ARM
+	LOCAL_SRC_FILES += \
+		src/opts/S32A_D565_Opaque_arm.S \
+		src/opts/S32A_Opaque_BlitRow32_arm.S \
+		src/opts/S32A_Blend_BlitRow32_arm.S
+endif
+
+ifeq "$(findstring armv6,$(TARGET_ARCH_VARIANT))" "armv6"
+	ARCH_ARMV6_ARMV7 := true
+endif
+
+ifeq "$(findstring armv7,$(TARGET_ARCH_VARIANT))" "armv7"
+	ARCH_ARMV6_ARMV7 := true
+endif
+
+ifeq ($(ARCH_ARMV6_ARMV7),true)
+	LOCAL_SRC_FILES += \
+		src/opts/S32_Opaque_D32_nofilter_DX_gether_arm.S
+endif
+
+
 LOCAL_SHARED_LIBRARIES := \
 	libcutils \
 	libemoji \
